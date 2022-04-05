b5dfe45df426157ca02937c5081d0c3a9efc926b
Tatsuya Kaneko <tatsuya.j.kaneko@gmail.com>
2022-04-05 15:11:02 +0200

    Initial Commit

---
+++ RijksCollection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RijksCollection
+//
+//  Created by Tatsuya Kaneko on 05/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
